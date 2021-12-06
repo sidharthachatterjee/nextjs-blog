1efbd9e7e9349af8c550dcaca03df52cfe73039a
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2021-12-06 17:36:44 +0530

    Add login

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,25 @@
+import { useState } from "react";
 import Head from "next/head";
 
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY
+);
+
 export default function Home() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    supabase.auth.signIn({
+      email,
+      password,
+    });
+  };
+
   return (
     <div>
       <Head>
@@ -10,6 +29,23 @@ export default function Home() {
 
       <main>
         <h1>Welcome Home</h1>
+        <form onSubmit={onSubmit}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="text"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button type="submit">Login</button>
+        </form>
       </main>
     </div>
   );
